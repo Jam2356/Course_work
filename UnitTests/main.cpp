ffc4a797e55ac505e0adb16d628807efac011306
Jam2356 <jam2000.ru@gmail.com>
2022-12-28 11:20:49 +0300

    UnitTests

---
+++ UnitTests/main.cpp
@@ -0,0 +1,60 @@
+#include <UnitTest++/UnitTest++.h>
+//#include "/home/stud/C++Projects/C++Project/Client1/main.cpp"
+#include "/home/stud/C++Projects/C++Project/Client1/Interface.cpp"
+#include "/home/stud/C++Projects/C++Project/Client1/ClassTCP.cpp"
+#include "/home/stud/C++Projects/C++Project/Client1/Client.cpp"
+#include "/home/stud/C++Projects/C++Project/Client1/Header.h"
+#include <string>
+#include <string.h>
+
+SUITE(TPort)
+{
+    TEST(nullPort) {
+        CHECK_THROW(ConnectTCP().connection(0),cipher_error);
+    }
+    TEST(lowPort) {
+        CHECK_THROW(ConnectTCP().connection(333),cipher_error);
+    }
+    TEST(bigPort) {
+        CHECK_THROW(ConnectTCP().connection(50000),cipher_error);
+    }
+}
+SUITE(TReturnValue)
+{
+    TEST(connection) {
+        ConnectTCP().sock;
+        CHECK_EQUAL(1,ConnectTCP().connection(33333));
+    }
+    TEST(connectionErr) {
+        ConnectTCP().sock;
+        CHECK_THROW(ConnectTCP().connection(3333),cipher_error);
+    }
+}
+SUITE(HASH)
+{
+    TEST(THash1) {
+        CHECK_EQUAL("F9C1E496823EBD52340672DC1CB2D87F",Client().takeHash("pass","salt"));
+    }
+    TEST(Hash2) {
+        CHECK_EQUAL("83234657C5DF8232839AC8C0572E158D",Client().takeHash("salt","pass"));
+    }
+}
+SUITE(TInterface)
+{
+    TEST(Interface1) {
+        string sourceFile = "/home/stud/Kursach/sadasdas";
+        CHECK_THROW(Interface().fileStrNumber(sourceFile),cipher_error);
+    }
+    TEST(Interface2) {
+        string sourceFile = "/home/stud/Kursach/sa";
+        CHECK_THROW(Interface().fileStrSize(sourceFile),cipher_error);
+    }
+    TEST(Interface3) {
+        string sourceFile = "/home/stud/Kursach/sa";
+        CHECK_THROW(Interface().fileVectData(sourceFile),cipher_error);
+    }
+}
+int main(int argc, char **argv)
+{
+    return UnitTest::RunAllTests();
+}
